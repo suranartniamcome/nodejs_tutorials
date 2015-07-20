bea326d768d304ebe2a1ec10286b38cdf7c43794
unknown <SuranartN@SuranartN-NB.sanookonline.co.th>
2015-07-20 17:33:58 +0700

    add simple CommonJS modules

---
+++ src/app.js
@@ -0,0 +1,6 @@
+var Engine = require('./lib/engine.js');
+var Engine2 = require('./lib/engine2');
+
+var e = new Engine('xxx');
+e.forward();
+// Engine.forward();
---
+++ src/lib/engine2/index.js
@@ -0,0 +1,24 @@
+var message = '';
+
+var Class = function(msg) {
+	message = msg;
+	console.log(message);
+}
+
+Class.prototype = {
+	forward: function() {
+		console.log("The car2 is movie forward" + message);
+	}
+}
+
+// var Class = {
+// 	forward: function() {
+// 		console.log("THe car is movie forward");
+// 	}
+// };
+
+module.exports = Class;
+
+// exports.forward = function() {
+// 	console.log("THe car is movie forward");
+// }
